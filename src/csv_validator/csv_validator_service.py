b69f759df56e73fd50cf4356b04ed1a85c073390
Harold Torrado <h.torrado@techko.com.au>
2025-09-30 16:52:18 +1000

    Rename CATL_OFFSET_REGISTER to CATL_SBMU_OFFSET_REGISTER for consistency in naming

--- src/csv_validator/csv_validator_service.py
+++ src/csv_validator/csv_validator_service.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 
 from model.required_headers_dto import RequiredHeadersDto
-from utils.catl_sbmu_offset_register_const import CATL_OFFSET_REGISTER
+from utils.catl_sbmu_offset_register_const import CATL_SBMU_OFFSET_REGISTER
 
 
 class CsvValidatorService:
@@ -59,7 +59,7 @@ class CsvValidatorService:
         if len(self.driverRequiredHeaders.SBMU) > 0:
             for stringIndex in range(0, self.numberOfStrings):
                 stringHeaders = [
-                    f"{int(header, 16) + (stringIndex * CATL_OFFSET_REGISTER):X}"
+                    f"{int(header, 16) + (stringIndex * CATL_SBMU_OFFSET_REGISTER):X}"
                     for header in self.driverRequiredHeaders.SBMU
                 ]
                 requiredHeaders.extend(stringHeaders)
