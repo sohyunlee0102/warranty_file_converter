6a52e10478bfecbc8f88b8a2150eebf2ab49ab4b
Harold Torrado <h.torrado@techko.com.au>
2025-09-30 17:09:54 +1000

    Refactor CsvValidatorService to streamline initialization and enhance validation process with progress feedback

--- src/__main__.py
+++ src/__main__.py
@@ -8,8 +8,6 @@ from file_manager.file_manager_service import FileManagerService
 from utils.cli.get_csv_path import getCsvPath
 from utils.cli.get_driver_model import getDriverModel
 from utils.get_driver_configuration import getDriverConfiguration
-from utils.get_number_of_strings import getNumberOfStrings
-from utils.get_required_headers import getRequiredHeaders
 from utils.normalize_headers import normalizeHeaders
 
 
@@ -19,21 +17,17 @@ def processCsvFile():
 
     csvPath = getCsvPath()
     csvData = fileManagerInstance.getCsvData(csvPath)
-    normalizeHeaders(csvData)
     print(f"Processing CSV file: {fileManagerInstance.getFileName(csvPath)}")
 
     driverModel = getDriverModel()
     print(f"Selected Driver Model: {driverModel.name}\n")
 
+    normalizeHeaders(csvData)
     driverConfig = getDriverConfiguration(driverModel)
-    requiredHeaders = getRequiredHeaders(driverConfig)
-    numberOfStrings = getNumberOfStrings(csvData)
 
     fileValidatorInstance = CsvValidatorService(
-        requiredHeaders,
         csvData,
-        numberOfStrings,
-        driverConfig.timeMaxIntervalInSeconds,
+        driverConfig,
     )
     fileValidatorInstance.validate(csvPath)
 
